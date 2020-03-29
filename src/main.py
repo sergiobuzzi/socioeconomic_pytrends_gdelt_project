8f8302e28f5045d9afb5575fe6589fc41c260649
albertovpd <a.vargas.pina@gmail.com>
2020-03-29 14:09:12 +0200

    timer and keywords 1 done

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import pytrends
 from pytrends.request import TrendReq
 import pandas as pd
-import time
+import time as timer #change the name to avoid erros with time from datetime
 import datetime
 from datetime import datetime, date, time
 # # Security
@@ -16,15 +16,21 @@ from datetime import datetime, date, time
 # auth.authenticate_user()
 # gc = gspread.authorize(GC.get_application_default())
 
-TrendReq(hl='es-ES', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80','https://35.201.123.31:880'], retries=2, backoff_factor=0.1)
 
-pytrend = TrendReq()
-pytrend.build_payload(
-    kw_list=["empleo","desempleo","crisis","corrupcion","centro de salud", "hospital"], 
-    timeframe='today 1-m', 
-    geo = 'ES')
+#this works
+pytrends = TrendReq(hl='ES', tz=0)
+kw_list = [
+    ["empleo","desempleo","hipoteca"],19,
+    ["centro de salud","hospital","coronavirus"],45,
+    ["corrupcion","juicio","politica"],19
+    ]
+for i in range(len(kw_list)):
+    if i%2==0:
+        print("Sleep 10 seconds between requesting ",str(kw_list[i]))
+        timer.sleep(10)
+        print("Done")
+        #print(kw_list[i],kw_list[i+1])
+        pytrends.build_payload(kw_list[i], cat=kw_list[i+1],timeframe='today 5-y', geo='ES', gprop='')
+        interest_over_time_df = pytrends.interest_over_time() 
+        print(interest_over_time_df.head())
 
-#to get interest over time score, you'll need pytrend.interest_over_time() function.
-#For more functions, check this: https://github.com/GeneralMills/pytrends
-interest_over_time_df = pytrend.interest_over_time() 
-print(interest_over_time_df.head())
